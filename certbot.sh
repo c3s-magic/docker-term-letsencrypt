61261d04740af36a8321b2decb353dcf63a8b294
Niels Drost <n.drost@esciencecente.nl>
2018-08-28 15:23:08 +0200

    using hostname for certificate, run frontend on port 80

--- certbot.sh
+++ certbot.sh
@@ -16,7 +16,7 @@ install_cert()
 
 if [ -z "$DIR_NAME" ]; then
 	# There is no existing certbot configuration
-	DOMAIN_PARAMS="-d $(echo $SSL_DOMAINS | sed 's/,/ -d /g')"
+	DOMAIN_PARAMS="-d $(echo $EXTERNAL_HOSTNAME | sed 's/,/ -d /g')"
 	( set -x; certbot certonly --webroot -w /acme -n --agree-tos --email ${SSL_ADMIN_EMAIL} ${DOMAIN_PARAMS} --expand )
 	if [ $? -ne 0 ]; then
 		echo "WARNING: Certbot failed to create certificate"
